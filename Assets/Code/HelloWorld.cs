4e5ff21033de9b24ec927b3be04d505de025471b
John Bruneau <artfailinc@gmail.com>
2025-03-03 19:22:17 -0500

    Revert "updated speed"
    This reverts commit d16a49330a5bce43cbeffd4940202df8fcf2ac86.

--- Assets/Code/HelloWorld.cs
+++ Assets/Code/HelloWorld.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class HelloWorld : MonoBehaviour
 {
-    int speed = 10;
+    int speed = 5;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
